6fad08c996ce17a1d07f159170b007c3bca83304
Ayush Nagar <ayushnagar15tfe@gmail.com>
2024-02-01 11:37:05 +0530

    Added styles using Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder, {
     action as createOrderAction,
 } from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
     {
         element: <AppLayout />,
         errorElement: <Error />,
+
         children: [
             {
                 path: "/",
@@ -22,20 +23,19 @@ const router = createBrowserRouter([
                 path: "/menu",
                 element: <Menu />,
                 loader: menuLoader,
+                errorElement: <Error />,
             },
+            { path: "/cart", element: <Cart /> },
             {
-                path: "/cart",
-                element: <Cart />,
+                path: "/order/new",
+                element: <CreateOrder />,
+                action: createOrderAction,
             },
             {
                 path: "/order/:orderId",
                 element: <Order />,
                 loader: orderLoader,
-            },
-            {
-                path: "/order/new",
-                element: <CreateOrder />,
-                action: createOrderAction,
+                errorElement: <Error />,
             },
         ],
     },
